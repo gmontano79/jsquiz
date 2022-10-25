8f17c2bd380b4fc8d8599218ff45993e40130af8
gmontano79 <gmontano79@gmail.com>
2022-10-25 14:53:29 -0400

    Completed the questions select answer and questions rotation

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,29 +1,17 @@
 var startCard = document.querySelector("div");
 var startBtn = document.getElementById("start");
 var timer = document.getElementById("clock");
-var q1 = document.getElementById("question1")
-
-var ulEl = document.querySelectorAll("ul")
-
-// CORRECT ANSWERS
-var answers =
-{
-  q1: "q1a3",
-  q2: "q2a2",
-  q3: "q3a4",
-  q4: "q4a3",
-  q5: "q5a4",
-}
+var container = document.getElementById("container")
+var score = 0
+var timeOver = document.getElementById("timeOver")
 
 
 //  ********* START BUTTON  ***************
 startBtn.addEventListener("click", countDown);
 
-
-
 startBtn.addEventListener("click", function () {
   startCard.style.display = "none";
-  q1.style.display = "block";
+  container.style.display = "block";
 
 })
 
@@ -37,7 +25,6 @@ function countDown() {
     if (timeLeft > 1) {
       timer.textContent = timeLeft;
       timeLeft--;
-      console.log(timeLeft);
     } else {
       timer.textContent = "0";
       clearInterval(timeInterval);
@@ -46,11 +33,50 @@ function countDown() {
   }, 1000);
 }
 
-// CLICK ANSWER FOR NEXT QUESTION 
-function checkAnswer() {
 
-  if ()
+
+// ************************************************
+// *******  QUESTIONS SCRIPT **********************
+
+let indexQuestion = 0
+
+loadQuestion(indexQuestion)
+
+
+function loadQuestion(index) {
+  objectQuestion = questions[index];
+  options = [...objectQuestion.distractions];
+  options.push(objectQuestion.answer);;
+  options.sort(() => Math.random() - 0.5);
+  document.getElementById("question1").innerHTML = objectQuestion.question
+  document.getElementById("option1").innerHTML = options[0];
+  document.getElementById("option2").innerHTML = options[1];
+  document.getElementById("option3").innerHTML = options[2];
+  document.getElementById("option4").innerHTML = options[3];
 }
+
+async function selectOption(index) {
+  let validation = options[index] === objectQuestion.answer
+  if (validation) {
+
+    document.getElementById("answer").innerHTML = "Correct answer!"
+    score++
+  } else {
+    document.getElementById("answer").innerHTML = "Wrong answer!"
+    score--
+
+  }
+
+  indexQuestion++;
+  if (indexQuestion === questions.length) {
+    indexQuestion = 0;
+    // document.getElementById("answer").style.display = "none"
+  }
+  loadQuestion(indexQuestion);
+}
+console.log(score);
+
+// ***************************************************************
 
 
 
---
+++ assets/script/questions.js
@@ -0,0 +1,41 @@
+let questions = [
+  {
+    question: "Commonly used data types DO Not Include:",
+    answer: "Alerts",
+    distractions: [
+      "Strings", "Booleans", "Numbers"
+    ],
+  },
+  {
+    question: "The condition in an if/else stament is enclosed with _________:",
+    answer: "Curly Brackets",
+    distractions: [
+      "Quotes", "Parenthesis", "Square Brackets"
+    ],
+  },
+  {
+    question: "Array in JavaScript can be used to store:",
+    answer: "All of the above",
+    distractions: [
+      "Numbers and Strings", "Other arrays", "Booleans"
+    ],
+  },
+  {
+    question: "String values must be enclosed within ________ when being assigned to variables.",
+    answer: "Quotes",
+    distractions: [
+      "Comas", "Curly Brackets", "Parenthesis"
+    ],
+  },
+  {
+    question: "A very usefull tool used during developmnet and debugging for print content to the debugger is:",
+    answer: "Console.log",
+    distractions: [
+      "Java Script", "Terminal / Bash", "For loops"
+    ],
+  },
+]
+
+
+
+
